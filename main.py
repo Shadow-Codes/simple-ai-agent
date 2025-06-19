0cb39e2fd9d77810401cc4197a4f8fb5f95117f1
Shadow-Codes <109313532+Shadow-Codes@users.noreply.github.com>
2025-06-19 19:01:09 +0530

    Initial setup: integrate Gemini API and add basic content generation functionality

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+# creates gemini developer API client
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash-001",
+    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+)
+
+metadata = response.usage_metadata
+
+print(response.text)
+print(f"Prompt tokens: {metadata.prompt_token_count}")
+print(f"Response tokens: {metadata.candidates_token_count}")
