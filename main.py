3db60700346d2ac4d11fa904c1549ea4ed77b8d2
Shadow-Codes <109313532+Shadow-Codes@users.noreply.github.com>
2025-06-19 21:45:54 +0530

    Accept and validate prompt from user
    - Accept prompt as a CLI argument instead of hardcoded value
    - Add check for valid CLI arguments
    - Exit with proper error message and error code

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 from dotenv import load_dotenv
 from google import genai
@@ -9,9 +10,14 @@ api_key = os.environ.get("GEMINI_API_KEY")
 # creates gemini developer API client
 client = genai.Client(api_key=api_key)
 
+# checks for prompt before making API call
+if len(sys.argv) < 2:
+    print("You need to enter a prompt")
+    sys.exit(1)
+
 response = client.models.generate_content(
     model="gemini-2.0-flash-001",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    contents=sys.argv[1],
 )
 
 metadata = response.usage_metadata
